98ec155b0d86a505728a0182c1ee46f3de6666c9
king <authurlunga@gmail.com>
2021-07-16 11:32:03 +0200

    Removed some heroku database settings

--- roomy/settings.py
+++ roomy/settings.py
@@ -1,14 +1,12 @@
 
 
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# dotenv_file = os.path.join(BASE_DIR, ".env")
-# if os.path.isfile(dotenv_file):
-#     dotenv.load_dotenv(dotenv_file)
+
 
 TEMPLATE_DIR = os.path.join(BASE_DIR,'roomy/templates')
 
@@ -156,6 +154,3 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-django_heroku.settings(locals())
-
-# del DATABASES['default']['OPTIONS']['sslmode']
